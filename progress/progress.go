371799948c1a661f172e6a24fbee273c7e5bc1f3
Raffrerty <raffsimms23@gmail.com>
2022-05-29 12:36:51 +0100

    Remove unused constant

--- progress/progress.go
+++ progress/progress.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-type values struct {
+type Values struct {
 	Year   float64 `json:"year"`
 	Month  float64 `json:"month"`
 	Week   float64 `json:"week"`
@@ -14,16 +14,15 @@ type values struct {
 	Minute float64 `json:"minute"`
 }
 
-type query struct {
+type Response struct {
 	Timezone  string `json:"timezone"`
 	Timestamp string `json:"timestamp"`
-	Result    values `json:"result"`
+	Result    Values `json:"result"`
 }
 
 const (
-	SECONDS_IN_DAY  = 86400
-	SECONDS_IN_HOUR = 3600
-	HOURS_IN_WEEK   = 168
+	SecondsInDay  = 86400
+	SecondsInHour = 3600
 )
 
 func isLeapYear(year int) bool {
@@ -36,7 +35,7 @@ func isLeapYear(year int) bool {
 }
 
 func daysInMonth(year int, month time.Month) float64 {
-	t := time.Date(year, time.Month(month), 0, 0, 0, 0, 0, time.UTC)
+	t := time.Date(year, month, 0, 0, 0, 0, 0, time.UTC)
 	return float64(t.AddDate(0, 1, 0).Day())
 }
 
@@ -48,22 +47,22 @@ func realWeekday(now time.Time) int {
 	}
 }
 
-func seconds(t time.Time) values {
+func seconds(t time.Time) Values {
 	minuteSeconds := float64(t.Second())
 	hourSeconds := float64(t.Minute()*60) + minuteSeconds
-	daySeconds := float64(t.Hour()*SECONDS_IN_HOUR) + hourSeconds
+	daySeconds := float64(t.Hour()*SecondsInHour) + hourSeconds
 
-	return values{
-		Year:   float64(t.YearDay()*SECONDS_IN_DAY) + daySeconds,
-		Month:  float64(t.Day()*SECONDS_IN_DAY) + daySeconds,
-		Week:   float64((realWeekday(t)-1)*SECONDS_IN_DAY) + daySeconds,
+	return Values{
+		Year:   float64(t.YearDay()*SecondsInDay) + daySeconds,
+		Month:  float64(t.Day()*SecondsInDay) + daySeconds,
+		Week:   float64((realWeekday(t)-1)*SecondsInDay) + daySeconds,
 		Day:    daySeconds,
 		Hour:   hourSeconds,
 		Minute: minuteSeconds,
 	}
 }
 
-func percentages(t time.Time) values {
+func percentages(t time.Time) Values {
 	v := seconds(t)
 
 	var daysInYear float64
@@ -75,35 +74,35 @@ func percentages(t time.Time) values {
 
 	daysInMonth := daysInMonth(t.Year(), t.Month())
 
-	v.Year = v.Year / (daysInYear * float64(SECONDS_IN_DAY)) * 100
-	v.Month = v.Month / (daysInMonth * float64(SECONDS_IN_DAY)) * 100
-	v.Week = v.Week / (168 * float64(SECONDS_IN_HOUR)) * 100
-	v.Day = v.Day / float64(SECONDS_IN_DAY) * 100
-	v.Hour = v.Hour / float64(SECONDS_IN_HOUR) * 100
+	v.Year = v.Year / (daysInYear * float64(SecondsInDay)) * 100
+	v.Month = v.Month / (daysInMonth * float64(SecondsInDay)) * 100
+	v.Week = v.Week / (168 * float64(SecondsInHour)) * 100
+	v.Day = v.Day / float64(SecondsInDay) * 100
+	v.Hour = v.Hour / float64(SecondsInHour) * 100
 	v.Minute = v.Minute / 60 * 100
 
 	return v
 }
 
-func Query(format string, timezone string) (query, error) {
+func Query(format string, timezone string) (Response, error) {
 	location, err := time.LoadLocation(timezone)
 	if err != nil {
-		return query{}, errors.New("invalid timezone")
+		return Response{}, errors.New("invalid timezone")
 	}
 
 	now := time.Now().In(location)
 
-	var result values
+	var result Values
 
 	if format == "second" {
 		result = seconds(now)
 	} else if format == "percent" {
 		result = percentages(now)
 	} else {
-		return query{}, errors.New("invalid format")
+		return Response{}, errors.New("invalid format")
 	}
 
-	return query{
+	return Response{
 		Timezone:  timezone,
 		Timestamp: now.Format(time.RFC1123),
 		Result:    result,
